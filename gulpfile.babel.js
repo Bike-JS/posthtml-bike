baf2ef8af55c690ffcf08f30d6e513c26025fd23
Oleksandr Khyrenko <Oleksandr.Khyrenko@innovecs.com>
2017-10-02 00:12:43 +0300

    Remove gulp, update package.json

--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-
-const SRC_PATH = './lib/index.js';
-const DIST_PATH = './dist';
-
-gulp.task('build', () => (
-  gulp.src(SRC_PATH).pipe(babel()).pipe(gulp.dest(DIST_PATH))
-));
