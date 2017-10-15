73c9a15b0616c007e9b3b105f933fbd67504e258
Oleksandr Khyrenko <Oleksandr.Khyrenko@innovecs.com>
2017-10-15 18:59:48 +0300

    Update version

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.AUTO_CLASSES = exports.AUTO_TAGS = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _postcss = require('postcss');
+
+var _postcss2 = _interopRequireDefault(_postcss);
+
+var _fs = require('fs');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const TAGS = ['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr'];
 
@@ -35,7 +44,18 @@ const DEFAULT_OPTIONS = {
   replaceElemTag: 'div',
   skipTags: ['b', 'strong', 'i', 'span', 'div', 'section'],
   autoTags: AUTO_TAGS,
-  autoClasses: AUTO_CLASSES
+  autoClasses: AUTO_CLASSES,
+  postcss: false
+};
+
+const POSTCSS_DEFAULT_OPTIONS = {
+  match: 'text/postcss',
+  plugins: [],
+  process: (css, node) => {
+    node.attrs.type = 'text/css';
+    node.content = ['\n', css];
+    return node;
+  }
 };
 
 const selector = (block, elem, modName, modVal) => `${block}${elem ? `__${elem}` : ''}${modName ? modVal ? `_${modName}_${modVal}` : `_${modName}` : ''}`;
@@ -43,7 +63,7 @@ const selector = (block, elem, modName, modVal) => `${block}${elem ? `__${elem}`
 const classWithMods = (block, elem, mods, className = `${block}__${elem}`) => [className, ...mods.map(([name, value]) => selector(block, elem, name, value))].join(' ');
 
 exports.default = (options = DEFAULT_OPTIONS) => {
-  options = Object.assign({}, DEFAULT_OPTIONS, options);
+  options = _extends({}, DEFAULT_OPTIONS, options);
 
   return tree => {
     const process = (node, parent) => {
@@ -96,6 +116,16 @@ exports.default = (options = DEFAULT_OPTIONS) => {
     };
 
     tree.match({ tag: options.tag }, process);
+
+    if (typeof options.postcss === 'object') {
+      options.postcss = _extends({}, POSTCSS_DEFAULT_OPTIONS, options.postcss);
+
+      tree.match({ tag: 'style', attrs: { type: options.postcss.match } }, node => {
+        return options.postcss.process((0, _postcss2.default)([...options.postcss.plugins]).process(node.content).toString(), node, options);
+      });
+
+      return node;
+    }
 
     return tree;
   };
